cde6a9a3053c779a2000aa9633a41f6421d6406f
Samantha Phipps <sammi.phipps@gmail.com>
2020-01-01 13:22:28 -0700

    Added state to App.js so that can switch between the QuizMePage and ManageQuestions components when the 'nav' is clicked. Also needed to add the nav and changed background that I previously did.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 import QuizMePage from './components/QuizMePage'
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>QuizMe</h1>
-      </header>
-      <main>
-        <QuizMePage />
-      </main>
-    </div>
-  );
+import ManageQuestions from './components/ManageQuestions'
+
+class App extends Component {
+  state = {
+    componentShowing: "Quiz"
+  }
+
+  handleNavClick = (value) => {
+    this.setState({ componentShowing: value})
+  } 
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <h1>QuizMe</h1>
+          <nav>
+            <ul>
+              <li onClick={event => this.handleNavClick("Quiz")}>Take Quiz</li>
+              <li onClick={event => this.handleNavClick("Manage Questions")}>Manage Questions</li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          {
+            (this.state.componentShowing == "Quiz")
+              ?<QuizMePage />
+              :<ManageQuestions />
+          }
+        </main>
+      </div>
+    )
+  }
 }
 
 export default App;
